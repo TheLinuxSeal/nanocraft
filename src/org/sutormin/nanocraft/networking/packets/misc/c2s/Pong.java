a3f2667c240e60dddcc258b21459dd8de5d41378
ThePythonSeal <alexander.pereyaslavets@gmail.com>
2026-09-05 13:47:17 -0700

    chunks part 1

--- src/org/sutormin/nanocraft/networking/packets/misc/c2s/Pong.java
+++ src/org/sutormin/nanocraft/networking/packets/misc/c2s/Pong.java
@@ -5,6 +5,7 @@ import io.netty.buffer.Unpooled;
 import org.sutormin.nanocraft.networking.NetworkPhase;
 import org.sutormin.nanocraft.networking.Networking;
 import org.sutormin.nanocraft.networking.coders.PacketIO;
+import org.sutormin.nanocraft.networking.packets.types.C2SPacket;
 
 public class Pong implements C2SPacket {
     public static short CONFIG_ID = 5;
